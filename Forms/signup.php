2f74f5201435a26edb531c3a136723c70eca3cca
EvansMaingi <Evans.Maingi@strathmore.edu>
2024-10-05 20:27:08 +0300

    sign up module

--- Forms/signup.php
+++ Forms/signup.php
@@ -2,16 +2,22 @@
 require_once "../includes/dbConnection.php";
 require_once "../Forms/SignUpForm.php";
 require_once "../includes/constants.php";
+require_once "../Classes/DisplayUsers.php";
 
-
-
+// Establish a database connection
 $db = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 $conn = $db->getConnection();
 
-
+// Handle form submission
 $signUpForm = new SignUpForm($conn);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $signUpForm->handleFormSubmission();
+}
+
+// Render the form
 $signUpForm->renderForm();
 
-
-$signUpForm->handleFormSubmission();
+// Display all users in a table
+$displayUsers = new DisplayUsers($conn);
+$displayUsers->displayUsersTable();
 ?>
